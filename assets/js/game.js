66dc2390f97556c3b245fa72d06d45a9f7c10c1e
bkfleet1 <bkfleet1@gmail.com>
2021-11-29 10:45:30 -0600

    java playername code

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+var name = "your name";
+console.log(name);
+
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
